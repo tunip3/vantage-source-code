8ec7cad85c6e50873a91e450dd1f8103a06f9b07
tunip3 <26260613+tunip3@users.noreply.github.com>
2019-04-22 23:23:45 +0100

    Add files via upload

---
+++ v2.0.1/editor-bridge.js
@@ -0,0 +1,35 @@
+const ipcRenderer = require('electron').ipcRenderer;
+
+// Keep a reference to Buffer before Electron deletes it.
+const Buffer = window.Buffer;
+
+let editor;
+
+window.vantage = {
+    Buffer: Buffer,
+    setEditor(e) {
+        editor = e;
+        ipcRenderer.sendToHost('editor', 'initialized');
+    },
+    openDevTools() {
+        ipcRenderer.sendToHost('editor', 'dev-tools');
+    },
+};
+
+ipcRenderer.addListener('editor', function(_, type, data) {
+    if (type === 'load') {
+        console.info('Loading save data...');
+        if (editor) {
+            Promise.resolve(editor.load(Buffer.from(data.buffer)))
+                .then(() => ipcRenderer.sendToHost('editor', 'loaded'))
+                .catch(e => ipcRenderer.sendToHost('editor', 'loaded', e));
+        }
+    } else if (type === 'save') {
+        console.info('Saving...');
+        if (editor) {
+            Promise.resolve(editor.save())
+                .then(data => ipcRenderer.sendToHost('editor', 'saved', undefined, data))
+                .catch(e => ipcRenderer.sendToHost('editor', 'saved', e));
+        }
+    }
+});
